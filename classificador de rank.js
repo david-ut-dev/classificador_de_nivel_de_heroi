522c9e29bdb300f8637039ecfcad6186bec95a35
david-ut-dev <shurikinaki@gmail.com>
2024-11-06 12:46:04 -0300

    mudança

--- classificador de rank.js
+++ classificador de rank.js
@@ -1,28 +1,27 @@
-let save = ("receptaculo de raisenberg" , 7563).split(",")
-let resultado = []
+let save = "receptaculo de raisenberg , 7563";
+let resultado = [];
 
-save.array.forEach(element => {
-
+save.split(";").forEach(item => {
     let [nome, quantidade] = item.split(",");
     quantidade = parseInt(quantidade);
 
     if (quantidade < 1000) {
-        resultado.push(`${nome}: ferro`)
-    } else if (quantidade >1001 && quantidade<2000){
-        resultado.push(`${nome}: bronze`)
-    }else if (quantidade>2001 && 5000<quantidade){
-        resultado.push(`${nome}: prata`)
-    }else if (quantidade >5001 && 7000<quantidade){
-        resultado.push(`${nome}: ouro`)
-    }else if (quantidade>7001 && 8000<quantidade){
-        resultado.push(`${nome}: platina`)
-    }else if (quantidade>8001 && 9000<quantidade){
-        resultado.push(`${nome}: Ascendente`)
-    }else if (quantidade>9001 && 10000<quantidade){
-        resultado.push(`${nome}: imortal`)
-    }else (quantidade>10001){
-        resultado.push(`${nome}: radiante`)
+        resultado.push(`${nome.trim()}: ferro`);
+    } else if (quantidade >= 1001 && quantidade <= 2000) {
+        resultado.push(`${nome.trim()}: bronze`);
+    } else if (quantidade >= 2001 && quantidade <= 5000) {
+        resultado.push(`${nome.trim()}: prata`);
+    } else if (quantidade >= 5001 && quantidade <= 7000) {
+        resultado.push(`${nome.trim()}: ouro`);
+    } else if (quantidade >= 7001 && quantidade <= 8000) {
+        resultado.push(`${nome.trim()}: platina`);
+    } else if (quantidade >= 8001 && quantidade <= 9000) {
+        resultado.push(`${nome.trim()}: Ascendente`);
+    } else if (quantidade >= 9001 && quantidade <= 10000) {
+        resultado.push(`${nome.trim()}: imortal`);
+    } else {
+        resultado.push(`${nome.trim()}: radiante`);
     }
-   
 });
-console.log("O Herói de nome" + save [0] +" está no nível de" + save [1])
+
+resultado.forEach(item => console.log(`O Herói de nome ${item.split(":")[0]} está no nível de ${item.split(":")[1].trim()}`));
